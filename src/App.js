2efdaabecdd327f8ae67f80a226659fe4ca26116
George L <jorge.lopez@newfold.com>
2022-11-14 22:07:55 -0400

    misc

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Home from './components/Home'
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 export default function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styles from './header.module.css'
 import MenuButton from './MenuButton'
 
